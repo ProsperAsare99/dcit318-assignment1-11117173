ac9621089189bee4f26ce62e863c379143e8a8c7
PROSPER SHAIBU ASARE <asareprosper143@gmail.com>
2025-07-29 19:39:18 +0000

    grade app progress

---
+++ GradeCalculatorApp/GradeCalculator.cs
@@ -0,0 +1,14 @@
+namespace GradeCalculatorApp
+{
+    public static class GradeCalculator
+    {
+        public static string CalculateLetterGrade(int grade)
+        {
+            if (grade >= 90) return "A";
+            if (grade >= 80) return "B";
+            if (grade >= 70) return "C";
+            if (grade >= 60) return "D";
+            return "F";
+        }
+    }
+}
